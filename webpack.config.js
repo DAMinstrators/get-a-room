4cd27744c6a2226bcfd09a46d4421112c9864b17
Anto Afarian <antoafarian@Antos-MacBook-Pro.local>
2017-01-03 21:52:07 -0800

    set up webpack dhani wants me to be descriptive

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+   entry: './src/components/app.jsx',
+	
+   output: {
+      path:'./public',
+      filename: 'bundle.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
